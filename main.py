3a9c66ca3bc4a6eb3cc859ea1764558d5c6a33c3
youdezhanghao <111890818+youdezhanghao@users.noreply.github.com>
2022-11-26 03:52:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_weather1():
   res1 = requests.get(url).json()
   muzi = res1['newslist'][0]
   #area 城市  week = 星期 weather = 今天天气  real = 当前温度  lowest = 最低气温  highest= 最高气温  wind = 风项  windsc = 风力 sunrise = 日出时间 sunset = 日落时间 pop = 降雨概率 tips = 穿衣建议 
-  return muzi['area'], muzi['week'], muzi['weather'], muzi['real'], muzi['lowest'], muzi['highest'], muzi['wind'], muzi['windsc'], muzi['sunrise'], muzi['sunset'], muzi['pop'],muzi['tips']
+  return muzi['area'], muzi['week'], muzi['weather'], muzi['real'], muzi['lowest'], muzi['highest'], muzi['wind'], muzi['windsc'], muzi['sunrise'], muzi['sunset'],muzi['tips']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -66,7 +66,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 # wea, temperature, highest, lowest = get_weather()
-area, week, weather, real, lowest, highest, wind, windsc, sunrise, sunset, pop, tips = get_weather1()
+area, week, weather, real, lowest, highest, wind, windsc, sunrise, sunset, tips = get_weather1()
 data = {
     "date1": {
         'value':'今天是：'
